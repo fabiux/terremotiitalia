329c8227c4ba2424746144146e8b1182a64b4739
fabiux <battormoros@gmail.com>
2016-10-31 15:35:40 +0100

    Improvements

--- retrieve_data.py
+++ retrieve_data.py
@@ -6,17 +6,17 @@ License: GNU/GPL version 3 (see file LICENSE)
 from time import sleep
 from lib.utils import save_events, get_events
 
-leapyears = ['2012', '2008', '2004', '2000', '1996', '1992']
-years = ['2015', '2014', '2013', '2012', '2011', '2010', '2009', '2008', '2007', '2006', '2005', '2004', '2003', '2002',
-         '2001', '2000', '1999', '1998', '1997', '1996', '1995', '1994', '1993', '1992', '1991', '1990']
+leapyears = [2012, 2008, 2004, 2000, 1996, 1992, 1988, 1984, 1980, 1976, 1972, 1968, 1964, 1960]
+startyear = 1995
+endyear = 1999
 months = {'12': 31, '11': 30, '10': 31, '09': 30, '08': 31, '07': 31, '06': 30, '05': 31, '04': 30, '03': 31, '02': 28,
           '01': 31}
 
-for y in years:
+for y in range(startyear, endyear + 1):
     for m in months.keys():
         days = months[m]
         if (y in leapyears) and (m == '02'):
             days += 1
         days = ('0' + str(days))[-2:]
-        save_events(get_events(month=y + '-' + m, daysinmonth=days))
+        save_events(get_events(month=str(y) + '-' + m, daysinmonth=days))
         sleep(20)
